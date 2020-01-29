084124eb4a67a256919f09791f5d2df4d03755c7
Hganavak <hganavak@gmail.com>
2020-01-29 16:28:28 +1300

    Added PUT method

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ app.post('/api/coffees', (req, res) => {
 
     if(result.error) {
         res.status(400).send(result.error.details[0].message);
+        return;
     }
+
     const coffee = {
         id: COFFEES.length + 1,
         name: req.body.name
@@ -45,5 +47,28 @@ app.post('/api/coffees', (req, res) => {
     res.send(coffee);
 });
 
+app.put('/api/coffees/:id', (req, res) => {
+    const coffee = COFFEES.find(c => c.id === parseInt(req.params.id));
+    if(!coffee) res.status(404).send('A coffee with the given ID was not found.');
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+    console.log(result);
+
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    coffee.name = req.body.name;
+    res.send(coffee);
+
+
+
+    
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
