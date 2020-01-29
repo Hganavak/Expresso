3800fb570d85b2d0f92af40074cafc654c11f709
Hganavak <hganavak@gmail.com>
2020-01-29 15:25:09 +1300

    Removed hardcoded PORT variable

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const app = express ();
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('yowzah');
+    res.send('Welcome to the most advanced coffee API in the world');
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.get('/api/coffees', (req, res) => {
+    res.send([
+        { id: 1, coffee: 'Colombian'}
+    ]);
+});
+
+
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
