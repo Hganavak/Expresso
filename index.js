d71f97ce93941810bf0323010ee68e1adcca8276
Hganavak <hganavak@gmail.com>
2020-01-29 15:38:49 +1300

    Added route params

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const COFFEES = [
+    { id: 1, name: 'Colombian' },
+    { id: 2, name: 'Italian' },
+    { id: 3, name: 'Ethiopian' },
+];
+
 app.get('/', (req, res) => {
     res.send('Welcome to the most advanced coffee API in the world');
 });
 
 app.get('/api/coffees', (req, res) => {
-    res.send([
-        { id: 1, coffee: 'Colombian'}
-    ]);
+    res.send(COFFEES);
 });
 
+app.get('/api/coffees/:id', (req, res) => {
+    const COFFEE = COFFEES.find(c => c.id === parseInt(req.params.id));
+    if(!COFFEE) res.status(404).send('A coffee with the given ID was not found.');
+    res.send(COFFEE);
+});
 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
